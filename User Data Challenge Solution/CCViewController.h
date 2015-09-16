bdf780b681efa9589b66917c0591b4a231bf5f53
Rob Lipp <lunarwild@gmail.com>
2015-09-16 07:59:35 -0700

    Initial Commit

---
+++ User Data Challenge Solution/CCViewController.h
@@ -0,0 +1,13 @@
+//
+//  CCViewController.h
+//  User Data Challenge Solution
+//
+//  Created by Rob Lipp on 2015-09-16.
+//  Copyright (c) 2015 Rob Lipp. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CCViewController : UIViewController
+
+@end
